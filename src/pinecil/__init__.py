4cf5d1f3d874ae71ab272fe7d39ef9a4f4d3fd74
daniele <qdaniele97@gmail.com>
2023-12-31 18:59:32 +0100

    Added __init__.py file and moved code to src/pinecil

---
+++ src/pinecil/__init__.py
@@ -0,0 +1,14 @@
+
+from .ble import (
+                   BLE,
+                   DeviceNotFoundException,
+                   DeviceDisconnectedException
+                 )
+
+'''
+from .pinecil import (
+                       Pinecil,
+                       InvalidSettingException,
+                       ValueOutOfRangeException
+                     )
+'''
